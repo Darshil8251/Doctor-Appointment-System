80c6878b11f8501e88bf8b4c429f057814105e27
Darshil8251 <106241346+Darshil8251@users.noreply.github.com>
2023-05-15 08:47:34 +0530

    final

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
 const { Mongoose, default: mongoose } = require("mongoose");
 mongoose.set('strictQuery', false);
 
-const url = "mongodb+srv://Darshil8251:Darshil8251@cluster0.ybuywdn.mongodb.net/";
+
 const dbname='doc_app';
+const url = "mongodb+srv://Darshil8251:Darshil8251@cluster0.ybuywdn.mongodb.net/doc_app";
 const connection = async () => {
-//  let client =new MongoClient(url);
-//  let result= await client.connect();
-//  let db=  result.db(dbname);
-//  return db.collection("User");
+
 try {
     await mongoose.connect(url,{useNewUrlParser: true});
-    // console.log(`Mongodb connected ${mongoose.connection.host}`);
 } catch (error) {
     console.log(`Mongodb Server Issue ${error}`);
 }
